76c83b651639cea95845ee0cc537c99ef89d52fc
joepasquale <jpasquale@umass.edu>
2021-02-17 08:37:53 -0500

    added website react template.
    cloned from https://github.com/jcoelho93/personal-website. thanks jose, you're a real one.

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,33 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/menu';
-import { PLACES } from './shared/places';
-import Greet from './components/greet';
+import React, { Component } from "react";
+import Header from "./components/structure/Header";
+import Content from "./components/structure/Content";
+import Footer from "./components/structure/Footer";
+import Resume from "./resume.json";
 
-class App extends React.Component {					
-  
-	constructor(props){
-		super(props);
-		
-		this.state = {
-			places: PLACES
-		};
-	}
-	
-	render(){
-		return (
-			<div className='App'>
-				<Navbar color="primary">
-					<div className="container" id="brand">
-						<NavbarBrand>Joe Pasquale</NavbarBrand>
-					</div>
-				</Navbar>
-				<Greet />
-			</div>
-		);
-	}
+class App extends Component {
+  componentDidMount() {
+    document.title = [
+      Resume.basics.name,
+      Resume.basics.label,
+      [Resume.basics.location.region, Resume.basics.location.country].join(", ")
+    ].join(" | ");
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Content />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
