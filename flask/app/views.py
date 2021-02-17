472b04f52dddd119a1149167ed517564660e120d
joepasquale <jpasquale@umass.edu>
2021-02-16 20:19:40 -0500

    added html content

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 from app import app
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/index')
+def index():
+    user = {'username': 'Joe'}
+    return render_template('index.html', title='Joe Pasquale', user=user)
